0750c4d62a047bfe240b7ae669184aac4c2a6f45
Robin9094707 <robin.nico.juhas.2003@gmail.com>
2026-08-25 20:09:21 +0200

    Add RJ Furz-App entry point

---
+++ App/RJFurzAppApp.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct RJFurzAppApp: App {
+    private let container: ModelContainer
+
+    init() {
+        do {
+            let schema = Schema([
+                FartEntry.self,
+                FartFolder.self,
+                FartReminder.self
+            ])
+            let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+            container = try ModelContainer(for: schema, configurations: [configuration])
+            DebugLogger.shared.log("App gestartet – SwiftData bereit")
+        } catch {
+            fatalError("SwiftData konnte nicht initialisiert werden: \(error)")
+        }
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+        .modelContainer(container)
+    }
+}
